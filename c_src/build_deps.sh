e8fe45f55993a90e8a39d54772b9bed3be82167f
Dave Smith <dizzyd@dizzyd.com>
2010-02-16 07:06:56 -0700

    Updating to latest rebar that also simplifies building in x-platform settings

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -21,25 +21,6 @@ case "$1" in
             patch -p0 < $x
         done
 
-        # Rebar sets up a custom env that we really don't want
-        unset CFLAGS CXXFLAGS LDFLAGS
-
-        # Use the ERLANG_TARGET var to setup some additional flags as necessary
-        if [[ $ERLANG_TARGET =~ darwin9.*-64$ ]]; then
-            echo "Setting flags for Leopard w/ 64-bit Erlang..."
-            export CFLAGS="-m64"
-            export LDFLAGS="-arch x86_64"
-
-        elif [[ $ERLANG_TARGET =~ darwin10.*-32$ ]]; then
-            echo "Setting flags for Snow Leopard w/ 32-bit Erlang..."
-            export CFLAGS="-m32"
-            export LDFLAGS="-arch i386"
-
-        elif [[ $ERLANG_TARGET =~ solaris.*-64$ ]]; then
-            echo "Setting flags for Solaris w/ 64-bit Erlang..."
-            export CFLAGS="-D_REENTRANT -m64"
-        fi
-
         (cd embedded_innodb-$INNO_VSN && \
             ./configure --disable-shared --enable-static --with-pic \
                         --prefix=$BASEDIR/innodb && \
